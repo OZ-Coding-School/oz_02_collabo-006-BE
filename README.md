72e60dbac9cfa544439e70825888f0f08d6dc2e5
bamjun <khy5116@naver.com>
2024-04-23 19:44:20 +0900

    user-update

--- README.md
+++ README.md
@@ -8,12 +8,27 @@
 ### 프로젝트 처음시작 설정  
 
 - poetry 설치하기  
+  - 설치완료 후 `poetry --version` 으로 설치 확인하기.  
+
+  - mac  
+  ```bash
+  curl -sSL https://install.python-poetry.org | python3 -
+  ```
+
+  - windows  
+  ```bash
+  curl -sSL https://install.python-poetry.org | python -
+  ```
+
+<br>
 
 - poetry 가상환경에 패키지 설치  
    
   ```bash
   poetry update
   ```
+
+<br>
 
 - 장고 마이그레이트  
 
@@ -25,16 +40,25 @@
   python manage.py migrate
   ```
 
+<br>
+
 - 장고 슈퍼유저 생성  
 
   ```bash
   python manage.py createsuperuser
   ```
 
+<br>
 
 
+---  
 
+# APIs  
 
-  
-  
+### users
 
+- 사용자 생성
+  - `/api/v1/user/`
+
+- 사용자 수정
+  - `/api/v1/user/<int:user_id>/`
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from users.views import UserView, Users
+from users.views import UserView, Users, UserUpdateAPIView, LoginAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 
 urlpatterns = [
     path('', Users.as_view()),
+    path('<int:user_id>/', UserUpdateAPIView.as_view(), name='user-update'),
+    path('login/', LoginAPIView.as_view(), name='user-login'),
+    path('sjwt/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('sjwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("sjwt/verify/", TokenVerifyView.as_view(), name='token_verify'),
 ]
+
