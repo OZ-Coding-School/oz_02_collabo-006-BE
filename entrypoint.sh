1c3d68b11eaac2936995cfdda01c9893abda219f
bamjun <khy5116@naver.com>
2024-04-29 17:22:58 +0900

    fix:add instruction of makemigrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
+python manage.py makemigrations
+
 # 데이터베이스 마이그레이션
 python manage.py migrate
 
 # 정적 파일 수집
 python manage.py collectstatic --noinput
 
-python manage.py create_my_superuser
+# python manage.py create_my_superuser
 
 # 서버 시작
 exec "$@"
