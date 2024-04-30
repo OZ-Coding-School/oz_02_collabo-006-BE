7db326aee3796f577b11c6d0cd50a96c6fd4e103
bamjun <khy5116@naver.com>
2024-04-30 13:43:36 +0900

    fix:test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+set -e
 
 python manage.py makemigrations
 
-# 데이터베이스 마이그레이션
+# 데이터베이스 마이그레이션 실행
 python manage.py migrate
 
 # 정적 파일 수집
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
+
 
 # python manage.py create_my_superuser
 
-# 서버 시작
+# 커맨드 라인에서 전달된 명령 실행 (CMD에서 정의된 명령)
 exec "$@"
