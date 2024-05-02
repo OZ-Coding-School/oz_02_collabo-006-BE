51aea2a12df32857182e213b3cbbdd40fc8afb22
bamjun <khy5116@naver.com>
2024-05-02 13:38:42 +0900

    fix:static

--- config/settings.py
+++ config/settings.py
@@ -162,7 +162,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+if osname_index == 'posix':
+    STATIC_URL = '/src/static/'
+else:
+    STATIC_URL = '/static/'
+    
+
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
