3028b84215bfa223339dc303a53b7c4d1a8d9c1b
bamjun <khy5116@naver.com>
2024-05-07 18:33:20 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# Wait for PostgreSQL to become available
-until psql -h "$DB_HOST" -U "$DB_USER" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
 
 python manage.py makemigrations
 
