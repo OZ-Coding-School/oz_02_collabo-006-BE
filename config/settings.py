64b9952557dd58127f25910974ac2c10cefc5275
bamjun <khy5116@naver.com>
2024-05-08 11:24:54 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -181,7 +181,8 @@ else:
     STATICFILES_DIRS = []
 
 
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = '/static/'
+# STATIC_ROOT = BASE_DIR / 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
