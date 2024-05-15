2b08c2142eabc755788cd1c606552ccb99df77ac
bamjun <khy5116@naver.com>
2024-05-15 10:37:19 +0900

    Feat:follower following create detail delete[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C

--- follow/urls.py
+++ follow/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from follow.views import FollwerCreate
+from follow.views import FollowerCreate, FollowerDetail, FollowerDelete, FollowingCreate, FollowingDelete, FollowingDetail, FollowingDelete
 
 urlpatterns = [
-    path('followers/create/', FollwerCreate.as_view(), name='follower-create'),
+    path('follower/create/', FollowerCreate.as_view(), name='follower-create'),
+    path('follower/<int:user_id>/', FollowerDetail.as_view(), name='follower-detail'),
+    path('follower/delete/', FollowerDelete.as_view(), name='follower-delete'),
+    path('following/create/', FollowingCreate.as_view(), name='following-create'),
+    path('following/<int:user_id>/', FollowingCreate.as_view(), name='following-detail'),
+    path('following/delete/', FollowingDelete.as_view(), name='following-delete'),
 ]
--- follow/views.py
+++ follow/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from follow.models import Follower, Following
 from follow.serializers import FollowerSerializer, FollowingSerializer
 from rest_framework.views import APIView
@@ -6,17 +6,94 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
-class FollwerCreate(APIView):
+class FollowerCreate(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        serializer = FollowerSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        try:
+            serializer = FollowerSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+class FollowerDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, user_id):
+        try:
+            follower_obj = get_object_or_404(Follower, pk=user_id)
+            serializer = FollowerSerializer(follower_obj)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        except Follower.DoesNotExist:
+            return Response({'error': 'Follower does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        except TypeError as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        
+class FollowerDelete(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            follower_obj = get_object_or_404(Follower, pk=request.data['user_id'])
+            follower_obj.delete()
+            return Response({'message': 'Follower deleted'}, status=status.HTTP_200_OK)
+        except Follower.DoesNotExist:
+            return Response({'error': 'Follower does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        except TypeError as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
+class FollowingCreate(APIView):
+    permission_classes = [IsAuthenticated]
 
+    def post(self, request):
+        try:
+            serializer = FollowingSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class FollowingDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, user_id):
+        try:
+            following_obj = get_object_or_404(Following, pk=user_id)
+            serializer = FollowingSerializer(following_obj)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        except Following.DoesNotExist:
+            return Response({'error': 'Following does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        except TypeError as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class FollowingDelete(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            following_obj = get_object_or_404(Following, pk=request.data['user_id'])
+            following_obj.delete()
+            return Response({'message': 'Following deleted'}, status=status.HTTP_200_OK)
+        except Following.DoesNotExist:
+            return Response({'error': 'Following does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        except TypeError as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # 팔로워 조회
 # user = User.objects.get(id=1)
